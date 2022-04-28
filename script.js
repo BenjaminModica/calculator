58cf649765e014a7b09fed1163026d2fd56d1872
Benjamin Modica <modica.benjamin@gmail.com>
2022-04-28 15:03:58 +0200

    added continuous calculations

--- script.js
+++ script.js
@@ -3,38 +3,53 @@ const buttons = document.querySelectorAll('button');
 let firstNbrString = '', secondNbrString = '', resultNbrString = '', operatorString = '';
 let operatorStrings = ['plus', 'times', 'divide', 'minus'];
 
-//0: wait for firstNbr
-//1: wait for secondNbr
-let state = 0;
+let waitForFirstNumber = true; //If this is true the next number pressed will be added to first number
+let cont = false; //Sets whether calculations continue on previous result or if a new calculation is underway
 
 const calcDisplay = document.querySelector('.calc');
 const resultDisplay = document.querySelector('.result');
 
+/*
+*Eventlistenes for all buttons on calculator. 
+*/
 buttons.forEach(button => button.addEventListener('click', () => {
 
-    if (button.id === 'equals') {
+    if (button.id === 'equals' && !cont && !waitForFirstNumber) { //Will show result if not pressed already and if not waiting for first number
         resultNbrString = operate(firstNbrString, secondNbrString, operatorString);
         updateDisplay();
         firstNbrString = '', secondNbrString = '', operatorString = '';
-        state = 0;
+        waitForFirstNumber = true;
+        cont = true;
     } else if (operatorStrings.includes(button.id)) {
+        if (cont) firstNbrString = resultNbrString;
+        cont = false; 
         operatorString = button.id;
-        state = 1;
+        waitForFirstNumber = false;
         updateDisplay();
-    } else if (!state) {
+    } else if (waitForFirstNumber && button.id !== 'equals') {
         firstNbrString += button.id;
+        cont = false;
         updateDisplay();
-    } else {
+    } else if (button.id !== 'equals'){
         secondNbrString += button.id;
         updateDisplay();
+    } else {
+        console.log('here');
+        firstNbrString = '', secondNbrString = '', resultNbrString = '', operatorString = '';
+        updateDisplay();
+        cont = false;
     }
 }));
 
+/*
+Updates top and bottom halfs of display with current operation and result
+*/
 function updateDisplay() {
     calcDisplay.textContent = `${firstNbrString} ${((operatorString === '') ? '' : operatorChars[operatorString])} ${secondNbrString}`;
     resultDisplay.textContent = resultNbrString;
 }
 
+//Object for converting operator strings to correct characters for display
 const operatorChars = {
     equals: '=',
     plus: '+',
@@ -43,6 +58,10 @@ const operatorChars = {
     divide: '÷',
 }
 
+/*
+Takes numbers and operator as string and calls for operation functions. 
+Returns result of operation
+*/
 function operate(a, b, operator) {
     switch (operator) {
         case 'plus':
